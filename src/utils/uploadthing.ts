0b57367b36444d32b463b757842ff08f84771909
thomasalbertoDev <12221355@bsi.ac.id>
2024-10-20 20:38:39 +0700

    uploadting api

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -1,9 +1,14 @@
-import { generateUploadButton, generateUploadDropzone, generateUploader } from "@uploadthing/react";
-import { generateReactHelpers } from "@uploadthing/react";
-import type { OurFileRouter } from "../../../../apps/isomorphic/src/app/api/uploadthing/core";
+import { OurFileRouter } from '@/app/api/uploadthing/core';
+import {
+  generateUploadButton,
+  generateUploadDropzone,
+  generateUploader,
+  generateReactHelpers,
+} from '@uploadthing/react';
 
 export const Uploader = generateUploader<OurFileRouter>();
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
 
-export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>();
