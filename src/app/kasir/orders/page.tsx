a3930a5ac3a9a46d25df6bba459d858254c8beb2
thomasalbertoDev <12221355@bsi.ac.id>
2024-11-01 13:21:37 +0700

    chore: add condition in orders page

--- src/app/kasir/orders/page.tsx
+++ src/app/kasir/orders/page.tsx
@@ -11,7 +11,7 @@ import toast from 'react-hot-toast';
 import { FaAnglesLeft } from 'react-icons/fa6';
 import { IoMdClose } from 'react-icons/io';
 import { IoBagCheckOutline } from 'react-icons/io5';
-import { ActionIcon, Button, Input, Modal, Title } from 'rizzui';
+import { ActionIcon, Button, EmptyBoxIcon, Input, Modal, Title } from 'rizzui';
 import { CheckoutOrdersRequest, OrdersType } from './shared/core/_models';
 import { checkout, getAllOrders } from './shared/core/_requests';
 import TableOrders from './shared/partials/table';
@@ -103,87 +103,106 @@ export default function Orders() {
         <div className="items-start">
           <div className="space-y-7 @5xl:col-span-8 @5xl:space-y-10 @6xl:col-span-7">
             <div className="pb-5">
-              <TableOrders data={ordersList} />
-              <div className="border-t border-muted pt-7 @5xl:mt-3">
-                <div className="ms-auto mt-10 max-w-lg space-y-6">
-                  <div className="flex justify-between font-medium">
-                    Subtotal <span>{formatToRupiah(total)}</span>
+              {ordersList.length === 0 ? (
+                <div className="mt-20 flex items-center justify-center">
+                  <div className="flex flex-col items-center text-center">
+                    <EmptyBoxIcon className="mt-10 h-32 w-32" />
+                    <Title as="h6" className="font-medium text-gray-400">
+                      Orders is empty
+                    </Title>
                   </div>
-                  <div className="flex justify-between font-medium">
-                    Delivery{' '}
-                    <span className="font-semibold italic text-amber-600">
-                      Free
-                    </span>
-                  </div>
-                  <div className="flex justify-between border-t border-muted pt-5 text-base font-semibold">
-                    Total <span>{formatToRupiah(total)}</span>
-                  </div>
-
-                  <Button
-                    onClick={() => setModalState(true)}
-                    className="mb-6 h-11 w-full gap-3 bg-green-600 hover:bg-green-700"
-                  >
-                    <IoBagCheckOutline />
-                    Checkout Orders
-                  </Button>
                 </div>
-              </div>
-
-              {/* Pindahkan Form ke dalam Modal untuk memastikan konteks yang benar */}
-              <Modal isOpen={modalState} onClose={() => setModalState(false)}>
-                <Form
-                  onSubmit={onSubmit}
-                  validationSchema={validationSchema as any}
-                >
-                  {({ register, formState: { errors } }) => (
-                    <div className="m-auto px-7 pb-8 pt-6">
-                      <div className="mb-7 flex items-center justify-between">
-                        <Title as="h5" className="font-semibold">
-                          Checkout Orders
-                        </Title>
-                        <ActionIcon
-                          size="sm"
-                          variant="text"
-                          onClick={() => setModalState(false)}
-                        >
-                          <IoMdClose className="h-auto w-6" strokeWidth={1.8} />
-                        </ActionIcon>
+              ) : (
+                <div>
+                  <TableOrders data={ordersList} />
+                  <div className="border-t border-muted pt-7 @5xl:mt-3">
+                    <div className="ms-auto mt-10 max-w-lg space-y-6">
+                      <div className="flex justify-between font-medium">
+                        Subtotal <span>{formatToRupiah(total)}</span>
                       </div>
-                      <div>
-                        <Input
-                          type="number"
-                          size="lg"
-                          label="Payment Amount *"
-                          placeholder="Input payment amount..."
-                          className="[&>label>span]:font-medium"
-                          inputClassName="text-sm"
-                          {...register('payment_amount')}
-                          error={errors.payment_amount?.message}
-                        />
-
-                        <Input
-                          type="text"
-                          size="lg"
-                          label="Seat Number *"
-                          placeholder="Input seat number..."
-                          className="mt-3 [&>label>span]:font-medium"
-                          inputClassName="text-sm"
-                          {...register('seat_number')}
-                          error={errors.seat_number?.message}
-                        />
-
-                        <Button
-                          type="submit"
-                          size="lg"
-                          className="col-span-2 mt-5 w-full"
-                        >
-                          Checkout Orders
-                        </Button>
+                      <div className="flex justify-between font-medium">
+                        Delivery{' '}
+                        <span className="font-semibold italic text-amber-600">
+                          Free
+                        </span>
                       </div>
+                      <div className="flex justify-between border-t border-muted pt-5 text-base font-semibold">
+                        Total <span>{formatToRupiah(total)}</span>
+                      </div>
+
+                      <Button
+                        onClick={() => setModalState(true)}
+                        className="mb-6 h-11 w-full gap-3 bg-green-600 hover:bg-green-700"
+                      >
+                        <IoBagCheckOutline />
+                        Checkout Orders
+                      </Button>
                     </div>
-                  )}
-                </Form>
-              </Modal>
+                  </div>
+
+                  {/* Pindahkan Form ke dalam Modal untuk memastikan konteks yang benar */}
+                  <Modal
+                    isOpen={modalState}
+                    onClose={() => setModalState(false)}
+                  >
+                    <Form
+                      onSubmit={onSubmit}
+                      validationSchema={validationSchema as any}
+                    >
+                      {({ register, formState: { errors } }) => (
+                        <div className="m-auto px-7 pb-8 pt-6">
+                          <div className="mb-7 flex items-center justify-between">
+                            <Title as="h5" className="font-semibold">
+                              Checkout Orders
+                            </Title>
+                            <ActionIcon
+                              size="sm"
+                              variant="text"
+                              onClick={() => setModalState(false)}
+                            >
+                              <IoMdClose
+                                className="h-auto w-6"
+                                strokeWidth={1.8}
+                              />
+                            </ActionIcon>
+                          </div>
+                          <div>
+                            <Input
+                              type="number"
+                              size="lg"
+                              label="Payment Amount *"
+                              placeholder="Input payment amount..."
+                              className="[&>label>span]:font-medium"
+                              inputClassName="text-sm"
+                              {...register('payment_amount')}
+                              error={errors.payment_amount?.message}
+                            />
+
+                            <Input
+                              type="text"
+                              size="lg"
+                              label="Seat Number *"
+                              placeholder="Input seat number..."
+                              className="mt-3 [&>label>span]:font-medium"
+                              inputClassName="text-sm"
+                              {...register('seat_number')}
+                              error={errors.seat_number?.message}
+                            />
+
+                            <Button
+                              type="submit"
+                              size="lg"
+                              className="col-span-2 mt-5 w-full"
+                            >
+                              Checkout Orders
+                            </Button>
+                          </div>
+                        </div>
+                      )}
+                    </Form>
+                  </Modal>
+                </div>
+              )}
             </div>
           </div>
         </div>
