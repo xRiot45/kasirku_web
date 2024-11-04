368155892a4519830f4238f514e2521a39c2bc1a
thomasalbertoDev <12221355@bsi.ac.id>
2024-11-04 13:31:53 +0700

    feat: print invoice

--- src/app/kasir/invoice/[id]/page.tsx
+++ src/app/kasir/invoice/[id]/page.tsx
@@ -93,7 +93,7 @@ export default function Invoice() {
     <>
       <PageHeader title={pageHeader.title} breadcrumb={pageHeader.breadcrumb}>
         <div className="mt-4 flex items-center gap-3 @lg:mt-0">
-          <PrintButton onClick={() => handlePrint()} />
+          <PrintButton onClick={() => handlePrint()} title="Print Invoice" />
         </div>
       </PageHeader>
 
--- src/components/print-button.tsx
+++ src/components/print-button.tsx
@@ -4,7 +4,11 @@ import cn from '@/utils/class-names';
 import { PiPrinterBold } from 'react-icons/pi';
 import { Button, ButtonProps } from 'rizzui';
 
-export default function PrintButton({ className, ...props }: ButtonProps) {
+export default function PrintButton({
+  className,
+  title,
+  ...props
+}: ButtonProps) {
   return (
     <Button
       variant="outline"
@@ -12,7 +16,7 @@ export default function PrintButton({ className, ...props }: ButtonProps) {
       {...props}
     >
       <PiPrinterBold className="me-1.5 h-[17px] w-[17px]" />
-      Print
+      {title}
     </Button>
   );
 }
