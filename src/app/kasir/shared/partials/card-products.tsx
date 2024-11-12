ee4c1d58a7d33a2442c970dda6c8ec4679674149
thomasalbertoDev <12221355@bsi.ac.id>
2024-11-12 09:17:55 +0700

    style: add style grayscale if product_status not available

--- src/app/kasir/shared/partials/card-products.tsx
+++ src/app/kasir/shared/partials/card-products.tsx
@@ -3,7 +3,7 @@ import cn from '@/utils/class-names';
 import { formatToRupiah } from '@/utils/formatRupiah';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Button, Text, Title } from 'rizzui';
+import { Button, Flex, Text, Title } from 'rizzui';
 
 interface PropTypes {
   data: IProducts[];
@@ -13,9 +13,14 @@ export default function CardProducts(props: PropTypes) {
   const { data } = props;
 
   return (
-    <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
+    <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-5">
       {data.map((product) => (
-        <div key={product.id} className={cn('pb-0.5')}>
+        <div
+          key={product.id}
+          className={cn('pb-0.5', {
+            'opacity-50 grayscale': product.product_status === 'Tidak Tersedia',
+          })}
+        >
           <div className="relative">
             <div className="relative mx-auto aspect-square w-full overflow-hidden rounded-lg bg-gray-100">
               <Image
@@ -31,9 +36,17 @@ export default function CardProducts(props: PropTypes) {
           </div>
 
           <div className="pt-3">
-            <Title as="h6" className="mb-1 truncate font-inter font-semibold">
-              {product.product_name}
-            </Title>
+            <Flex justify="between" align="center">
+              <Title as="h6" className="mb-1 truncate font-inter font-semibold">
+                {product.product_name}
+              </Title>
+
+              <Title
+                className={`mb-1 truncate font-inter text-xs font-medium ${product?.product_status === 'Tersedia' ? 'text-green-500' : 'text-red-500'}`}
+              >
+                {product.product_status}
+              </Title>
+            </Flex>
 
             <Text as="p" className="truncate">
               {product.product_category.product_category_name}
@@ -42,11 +55,17 @@ export default function CardProducts(props: PropTypes) {
               {formatToRupiah(Number(product.product_price))}
             </div>
             <div className="mt-3">
-              <Link href={`/kasir/product/${product.id}`}>
-                <Button className="w-full" variant="outline">
+              {product.product_status === 'Tersedia' ? (
+                <Link href={`/kasir/product/${product.id}`}>
+                  <Button className="w-full" variant="outline">
+                    View Product
+                  </Button>
+                </Link>
+              ) : (
+                <Button className="w-full" variant="outline" disabled>
                   View Product
                 </Button>
-              </Link>
+              )}
             </div>
           </div>
         </div>
